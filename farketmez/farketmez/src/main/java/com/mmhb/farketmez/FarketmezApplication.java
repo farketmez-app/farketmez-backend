0bcb568560793892e6f59e768d3f7bfb1d4c9faa
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2023-09-15 22:16:16 +0300

    backend için base oluşturuldu

---
+++ farketmez/farketmez/src/main/java/com/mmhb/farketmez/FarketmezApplication.java
@@ -0,0 +1,13 @@
+package com.mmhb.farketmez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarketmezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarketmezApplication.class, args);
+	}
+
+}
---
+++ farketmez/farketmez/src/test/java/com/mmhb/farketmez/FarketmezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmhb.farketmez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarketmezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
