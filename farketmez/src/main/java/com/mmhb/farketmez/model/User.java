604db5ffc73b05f6656539430781d8905fbf04d4
w1that <marmarisco860@gmail.com>
2023-10-23 18:55:25 +0300

    feat(User): make user Entity operates basic CRUD operations

--- farketmez/src/main/java/com/mmhb/farketmez/model/User.java
+++ farketmez/src/main/java/com/mmhb/farketmez/model/User.java
@@ -10,19 +10,23 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String username;
 	private String password;
+	@Column(name = "name_surname")
 	private String nameSurname;
 	private Integer age;
 	private Integer gender;
@@ -41,4 +45,14 @@ public class User {
 	@ManyToOne
 	@JoinColumn(name = "user_type_id")
 	private UserType userType;
+
+	public User(String username, String password, String nameSurname, int age, int gender, String longitude, String latitude) {
+		this.username = username;
+		this.password = password;
+		this.nameSurname = nameSurname;
+		this.age = age;
+		this.gender = gender;
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
 }
