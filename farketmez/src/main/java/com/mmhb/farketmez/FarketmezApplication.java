41e4a4507fbf681216ec57bd0e388e5f747354f1
w1that <marmarisco860@gmail.com>
2023-10-23 18:54:01 +0300

    chore: ignore SecurityAutoConfiguration class

--- farketmez/src/main/java/com/mmhb/farketmez/FarketmezApplication.java
+++ farketmez/src/main/java/com/mmhb/farketmez/FarketmezApplication.java
@@ -3,13 +3,16 @@ package com.mmhb.farketmez;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @ComponentScan(basePackages = { "com.mmhb.farketmez.*" })
+// ignore security auto configuration to make requests in Postman without 401 Unauthorized error
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class FarketmezApplication {
+
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
