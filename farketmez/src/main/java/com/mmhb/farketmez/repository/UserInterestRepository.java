2f1bbbe1d97fc211efcf53dfb16d00be990f9c12
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2023-12-15 00:50:17 +0300

    kullanıcların interest bilgileri listelendi

--- farketmez/src/main/java/com/mmhb/farketmez/repository/UserInterestRepository.java
+++ farketmez/src/main/java/com/mmhb/farketmez/repository/UserInterestRepository.java
@@ -1,10 +1,18 @@
 package com.mmhb.farketmez.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.mmhb.farketmez.model.Interest;
 import com.mmhb.farketmez.model.UserInterest;
 
 @Repository
 public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
+	List<UserInterest> findByUserId(Long userId);
+
+	@Query("SELECT i FROM Interest i JOIN UserInterest ui ON i.id = ui.interestId WHERE ui.userId = :userId")
+	List<Interest> findInterestsByUserId(Long userId);
 }
