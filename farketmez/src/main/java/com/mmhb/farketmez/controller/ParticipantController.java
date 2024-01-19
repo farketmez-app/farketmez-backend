a8af0a0039fc02dca4cff1051ff33b9960ce35e4
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2024-01-19 18:27:16 +0300

    eksik anotasyonlar eklendi

--- farketmez/src/main/java/com/mmhb/farketmez/controller/ParticipantController.java
+++ farketmez/src/main/java/com/mmhb/farketmez/controller/ParticipantController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/participants")
 public class ParticipantController {
 
--- farketmez/src/main/java/com/mmhb/farketmez/controller/SettingsController.java
+++ farketmez/src/main/java/com/mmhb/farketmez/controller/SettingsController.java
@@ -2,6 +2,7 @@ package com.mmhb.farketmez.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/settings")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class SettingsController {
 
